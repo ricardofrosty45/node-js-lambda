0ff547ebc453ed743cc9b5c8c50205b28811c99d
Luan Ricardo De Luna Vieira Venancio <luan_ricardo2@hotmail.com>
2021-09-07 21:29:29 -0300

    Create And Two Types Of Read Done

--- src/functions/DynamoFunctions.js
+++ src/functions/DynamoFunctions.js
@@ -1,28 +1,27 @@
 var AWS = require('aws-sdk');
 const TABLE_NAME = "AuctionsTable";
 
-module.exports.putItem = {
-    putItem: async function putItemIntoDatabase(resultInsert,item){
-        console.log("chegou!!! entrou!");
-        var docClient = new AWS.DynamoDB.DocumentClient();
-        var params = {
-            TableName: TABLE_NAME,
-            Item: item
-        };
-        console.log(params);
-        var result = "";
-        await docClient.put(params).promise().then(function(data) {
-            console.log("data", data);
-            result = { status : 200,
-                      result: "Sucess",
-                      id: item.id, 
-                      message: "Item has been inserted with success"};
-        }).catch(function(err){
-            console.error("Error : ", err);
-            result = { status : 200,
-                      result : "Error",
-                      message: err};
-        });
-        resultInsert(result);
-      }
-  };
+async function putItem(resultInsert,item){
+
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    var params = {
+        TableName: TABLE_NAME,
+        Item: item
+    };
+    console.log(params);
+    var result = "";
+    await docClient.put(params).promise().then(function(data) {
+        console.log("data", data);
+        result = { status : 200,
+                  result: "Sucess",
+                  id: item.id, 
+                  message: "Item has been inserted with success"};
+    }).catch(function(err){
+        console.error("Error : ", err);
+        result = { status : 200,
+                  result : "Error",
+                  message: err};
+    });
+    
+  }
+module.exports.putItem = putItem;
